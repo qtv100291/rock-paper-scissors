208d6c2c0e1446741f91d3a5028f33e3207b2a91
qtv91 <quangtuanvu1991@gmail.com>
2020-08-10 10:15:03 +0700

    Updates

---
+++ precache-manifest.c58fe0af4b29b15bd1d8bd17e00dd734.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ac8a8c79816b6d7bd0f35f96563d74b",
+    "url": "/rock-paper-scissors/index.html"
+  },
+  {
+    "revision": "8bdb87ff6c5bf53be6b5",
+    "url": "/rock-paper-scissors/static/css/main.a54ff8a1.chunk.css"
+  },
+  {
+    "revision": "3f35651eb065259559e1",
+    "url": "/rock-paper-scissors/static/js/2.17ed1925.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/rock-paper-scissors/static/js/2.17ed1925.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8bdb87ff6c5bf53be6b5",
+    "url": "/rock-paper-scissors/static/js/main.5d0b1bbc.chunk.js"
+  },
+  {
+    "revision": "571aa2c2dceba1dcace3",
+    "url": "/rock-paper-scissors/static/js/runtime-main.662e4c70.js"
+  },
+  {
+    "revision": "9bbddf0c3d3d9baa80e7e7084207ac9c",
+    "url": "/rock-paper-scissors/static/media/rock.9bbddf0c.png"
+  },
+  {
+    "revision": "f4164fff21ea5ef90184f7729b2e8037",
+    "url": "/rock-paper-scissors/static/media/scissors.f4164fff.png"
+  }
+]);
